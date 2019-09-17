44d8f3083ac5eca40d266c337c95043d47c6f7ea
Justin Po <justin.andre.po@gmail.com>
2019-09-17 14:42:46 +0800

    added favicon

---
+++ public/precache-manifest.441ff61fcc5479dcef29a74727993371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613451e3e72512910072281652b123b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e45d7bc854e364b4aaaa",
+    "url": "/static/css/main.b8ff7e17.chunk.css"
+  },
+  {
+    "revision": "47a8d3f96a80182f4cdc",
+    "url": "/static/js/2.8f36454d.chunk.js"
+  },
+  {
+    "revision": "e45d7bc854e364b4aaaa",
+    "url": "/static/js/main.03319290.chunk.js"
+  },
+  {
+    "revision": "e17242ad271ca7d7755b",
+    "url": "/static/js/runtime~main.2368a5b5.js"
+  },
+  {
+    "revision": "e9d0411191333e7644e0336b17243d29",
+    "url": "/static/media/hero.e9d04111.png"
+  },
+  {
+    "revision": "f3f782829e02e394dbbe361ff9ce2984",
+    "url": "/static/media/logo.f3f78282.png"
+  }
+]);
---
+++ public/precache-manifest.a6029272da304b7bb3677d649fb06cc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989b6e9a3ccec37ed4636842a3275916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56cb5acc061e4b4bf82a",
+    "url": "/static/css/main.9326805a.chunk.css"
+  },
+  {
+    "revision": "47a8d3f96a80182f4cdc",
+    "url": "/static/js/2.8f36454d.chunk.js"
+  },
+  {
+    "revision": "56cb5acc061e4b4bf82a",
+    "url": "/static/js/main.03319290.chunk.js"
+  },
+  {
+    "revision": "e17242ad271ca7d7755b",
+    "url": "/static/js/runtime~main.2368a5b5.js"
+  },
+  {
+    "revision": "e9d0411191333e7644e0336b17243d29",
+    "url": "/static/media/hero.e9d04111.png"
+  },
+  {
+    "revision": "f3f782829e02e394dbbe361ff9ce2984",
+    "url": "/static/media/logo.f3f78282.png"
+  }
+]);
